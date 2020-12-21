05b7e5bffd4f6d747e673bb866befe60911af06c
Adnit Kamberi <adnit@Adnits-iMac.local>
2020-12-21 04:39:43 +0100

    made a alpha version of slideshow and tinkered with some CSS of navbar

---
+++ scripts/slideshow.js
@@ -0,0 +1,25 @@
+let slidePos = 1;
+showSlide(slidePos);
+
+function showSlide(n) {
+  let slides = document.getElementsByClassName("slide");
+  let i;
+  if (n < 1) {
+    slidePos = slides.length;
+  }
+  if (n > slides.length) {
+    slidePos = 1;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slidePos - 1].style.display = "block";
+}
+
+function setSlide(n) {
+  showSlide((slidePos += n));
+}
+
+function setCurrentSlide(n) {
+  showSlide((slidePos = n));
+}
