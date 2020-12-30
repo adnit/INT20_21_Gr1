527cbdb5749249fa81bb1475d1afe07f820e783d
albiongit <albion.ademi1@studenti.uni-pr.edu>
2020-12-30 01:15:56 +0100

    made popups and js selections

--- scripts/Validation.js
+++ scripts/Validation.js
@@ -27,10 +27,57 @@ document.querySelector('.close2').addEventListener('click',function(){
 document.getElementById('create-button').addEventListener('click',function(){
 
     document.querySelector('.bg-modal2').style.display = 'flex';
-    document.querySelector('.bg-modal1').style.display = 'none';
+    document.querySelector('.bg-modal').style.display = 'none';
 });
 
 document.querySelector('.close2').addEventListener('click',function(){
      document.querySelector('.bg-modal2').style.display='none';
+
+});
+
+//hall1 modal
+document.getElementById('hall1').addEventListener('click',function(){
+
+    document.querySelector('.hall1-modal').style.display = 'flex';
+});
+
+document.querySelector('.closehall1').addEventListener('click',function(){
+     document.querySelector('.hall1-modal').style.display='none';
+
+});
+
+
+//hall2 modal
+document.getElementById('hall2').addEventListener('click',function(){
+
+    document.querySelector('.hall2-modal').style.display = 'flex';
+});
+
+document.querySelector('.closehall2').addEventListener('click',function(){
+     document.querySelector('.hall2-modal').style.display='none';
+
+});
+
+
+//hall3 modal
+document.getElementById('hall3').addEventListener('click',function(){
+
+    document.querySelector('.hall3-modal').style.display = 'flex';
+});
+
+document.querySelector('.closehall3').addEventListener('click',function(){
+     document.querySelector('.hall3-modal').style.display='none';
+
+});
+
+
+//hall4 modal
+document.getElementById('hall4').addEventListener('click',function(){
+
+    document.querySelector('.hall4-modal').style.display = 'flex';
+});
+
+document.querySelector('.closehall4').addEventListener('click',function(){
+     document.querySelector('.hall4-modal').style.display='none';
 
 });
