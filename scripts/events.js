8a5210b8d7e03a33b689de56099598016d0dc9b9
dorunt1na3 <62035237+dorunt1na3@users.noreply.github.com>
2021-01-05 17:42:27 +0100

    Update events.js

--- scripts/events.js
+++ scripts/events.js
@@ -24,3 +24,52 @@ function closeForm() {
   document.getElementById("foto").style.display = "none";
   document.getElementById("ob").style.display = "block";
 }
+function dyjat(event, emriEventit, id) {
+hapEventin(event, emriEventit);
+pauseAllExcept(id);
+}
+
+
+let audios = [
+  document.getElementById("audio1"),
+  document.getElementById("audio2"),
+  document.getElementById("audio3"),
+  document.getElementById("audio4"),
+  document.getElementById("audio5"),
+  document.getElementById("audio6"),
+];
+function pauseAllExcept(audioId) {
+
+  let audioX = document.getElementById(audioId);
+  audios.forEach((element) => {
+    if (element == audioX) {
+      audioX.volume = 0.04;
+      audioX.play();
+      return;
+    }
+    element.pause();
+  });
+}
+
+
+
+var data = new Date("Jan 1, 2022 00:00:00").getTime();
+var x = setInterval(function() {
+
+  var sot = new Date().getTime();
+
+  var distance = data - sot;
+
+  var ditet = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var ora = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minuta = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var sekonda = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML = ditet + "d " + ora + "h "
+  + minuta + "m " + sekonda + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "URIME VITI I RI!";
+  }
+}, 1000);
