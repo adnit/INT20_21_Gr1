0a3e0e5a28d162ade205bf70e5b3924eaba07902
adniti <adnit.kamberi@student.uni-pr.edu>
2021-01-08 23:26:38 +0100

    fixed loading animation when loading movie details
    previously the loading animation was just a .gif, now its replaced with Eye of Sauron animation

--- scripts/movie.js
+++ scripts/movie.js
@@ -1,17 +1,17 @@
-const API_KEY = "AIzaSyCcbyvSc4f5CvRwdimTbSioPY7uPYBQB4Q";
-let trailerFrame = document.getElementById("trailer");
-let poster = document.getElementById("poster");
-let title = document.getElementById("title");
-let releasedRating = document.getElementById("releasedRating");
-let runtimeGenre = document.getElementById("runtimeGenre");
-let director = document.getElementById("director");
-let actors = document.getElementById("actors");
-let plot = document.getElementById("plot");
-let loadingScreen = document.getElementById("loadingScreen");
-let movieDetailsScreen = document.getElementById("movieGeneratedDetails");
-let trailerBlock = document.getElementById("trailerBlock");
-let trailerTitle = document.getElementById("trailer-title");
-let term = "";
+const API_KEY = 'AIzaSyCcbyvSc4f5CvRwdimTbSioPY7uPYBQB4Q';
+let trailerFrame = document.getElementById('trailer');
+let poster = document.getElementById('poster');
+let title = document.getElementById('title');
+let releasedRating = document.getElementById('releasedRating');
+let runtimeGenre = document.getElementById('runtimeGenre');
+let director = document.getElementById('director');
+let actors = document.getElementById('actors');
+let plot = document.getElementById('plot');
+let loadingScreen = document.getElementById('loadingScreen');
+let movieDetailsScreen = document.getElementById('movieGeneratedDetails');
+let trailerBlock = document.getElementById('trailerBlock');
+let trailerTitle = document.getElementById('trailer-title');
+let term = '';
 
 function generateDetails() {
   try {
@@ -20,45 +20,45 @@ function generateDetails() {
     alert(error);
   }
 
-  movieDetailsScreen.style.visibility = "visible";
-  trailerBlock.style.visibility = "visible";
-  loadingScreen.style.display = "none";
+  movieDetailsScreen.style.visibility = 'visible';
+  trailerBlock.style.visibility = 'visible';
+  loadingScreen.style.display = 'none';
 }
 
 function checkURL() {
   let url = window.location.href;
-  let movieID = url.substring(url.indexOf("?") + 1, url.length);
+  let movieID = url.substring(url.indexOf('?') + 1, url.length);
   return movieID;
 }
 
 function getMovieDetails(movieID) {
   $.getJSON(
-    "https://www.omdbapi.com/?",
-    { apikey: "af2beff5", i: movieID },
+    'https://www.omdbapi.com/?',
+    { apikey: 'af2beff5', i: movieID },
     (movieData) => {
-      if (movieData.Response != "True") {
+      if (movieData.Response != 'True') {
         console.error(movieData.Error);
         return;
       }
       document.title = movieData.Title;
-      $(poster).attr("src", movieData.Poster);
+      $(poster).attr('src', movieData.Poster);
       $(title).html(movieData.Title);
       term = movieData.Title;
-      $(releasedRating).html(movieData.Year + " | " + movieData.imdbRating);
-      $(runtimeGenre).html(movieData.Runtime + " | " + movieData.Genre);
+      $(releasedRating).html(movieData.Year + ' | ' + movieData.imdbRating);
+      $(runtimeGenre).html(movieData.Runtime + ' | ' + movieData.Genre);
       $(director).html(movieData.Director);
       $(actors).html(movieData.Actors);
       $(plot).html(movieData.Plot);
-      $(trailerTitle).html("Traileri i " + movieData.Title);
+      $(trailerTitle).html('Traileri i ' + movieData.Title);
       console.log(term);
       $.getJSON(
-        "https://www.googleapis.com/youtube/v3/search?",
-        { key: API_KEY, type: "video", part: "id", maxResults: 1, q: term },
+        'https://www.googleapis.com/youtube/v3/search?',
+        { key: API_KEY, type: 'video', part: 'id', maxResults: 1, q: term },
         (trailer) => {
           let videoTag = trailer.items[0].id.videoId;
           $(trailerFrame).attr(
-            "src",
-            "https://www.youtube.com/embed/" + videoTag
+            'src',
+            'https://www.youtube.com/embed/' + videoTag
           );
         }
       );
@@ -66,4 +66,4 @@ function getMovieDetails(movieID) {
   );
 }
 
-setTimeout(generateDetails, 2000);
+setTimeout(generateDetails, 3000);
