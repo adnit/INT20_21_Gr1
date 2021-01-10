b0239ac25a959b0bb340e2e0e6b6f88c6c7466e3
Artin <artinsermaxhaj18@gmail.com>
2021-01-10 23:55:04 +0100

    Ndryshime tvogla

--- scripts/objektet.js
+++ scripts/objektet.js
@@ -42,3 +42,39 @@ var Automobili = {
         }
     }
     Shkolla.nrnxenesvetotal;
+function Projekt(nxenes, lenda, ora){
+    this.nxenes = nxenes;
+    this.lenda = lenda;
+    this.ora = ora;
+}
+var Projekti = new Projekt(4, "Interneti",220);
+Projekt.prototype.Mesazh = function(){
+    console.log("Ne projektin nga lenda: " + this.lenda + " moren pjese "+ this.nxenes
+    + " ku ata punuan " + this.ora + " ore.");
+
+}
+Projekti.Mesazh();
+function Printo(x1,x2){
+    if(x1 * x2 > Number.MAX_VALUE){
+        console.log("Ky numer nuk mund te kuptohet nga JS.");
+    }
+    else { console.log("Ky numer eshte i perfshire ne domenin e JS.");}
+}
+console.log(isNaN("asndnasd"));
+console.log(isNaN(123));
+console.log(Printo.toString());
+console.log(Projekt.toString());
+function KuptimiEksponentit(x1){
+    console.log("Numri pa perdorur metoden toExponential(): "+ x1);
+    console.log("Numri me perdorimin e metodes toExponential(): " + x1.toExponential(2));
+}
+KuptimiEksponentit(3123.213213123);
+const p = "Perdorimi i metodes replace";
+const regex = /Perdorimi/g;
+console.log(p.replace(regex,"Gabim"));
+const regex1 = /Perdorimi/i;
+console.log(p.replace(regex1, "Perdorimi i sakte"));
+let kaliumi = /Perdorim/s;
+let sps = console.log(kaliumi.exec("Perdorimi i metodes exec ne JS"));
+let match = /[A-Z]/s;
+console.log(p.match(match));
