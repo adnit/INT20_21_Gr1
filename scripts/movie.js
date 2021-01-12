04f6c05bed72924925150a9c03df53b5f2b62682
adniti <adnit.kamberi@student.uni-pr.edu>
2021-01-12 21:59:16 +0100

    adjusted some things just a tad bit

--- scripts/movie.js
+++ scripts/movie.js
@@ -1,4 +1,6 @@
-const API_KEY = 'AIzaSyCcbyvSc4f5CvRwdimTbSioPY7uPYBQB4Q';
+const ombdAPI_KEY = 'af2beff5';
+const googleAPI_KEY = 'AIzaSyCcbyvSc4f5CvRwdimTbSioPY7uPYBQB4Q';
+
 let trailerFrame = document.getElementById('trailer');
 let poster = document.getElementById('poster');
 let title = document.getElementById('title');
@@ -14,12 +16,7 @@ let trailerTitle = document.getElementById('trailer-title');
 let term = '';
 
 function generateDetails() {
-  try {
-    getMovieDetails(checkURL());
-  } catch (error) {
-    alert(error);
-  }
-
+  getMovieDetails(checkURL());
   movieDetailsScreen.style.visibility = 'visible';
   trailerBlock.style.visibility = 'visible';
   loadingScreen.style.display = 'none';
@@ -28,17 +25,19 @@ function generateDetails() {
 function checkURL() {
   let url = window.location.href;
   let movieID = url.substring(url.indexOf('?') + 1, url.length);
+  if (movieID.length > 10) {
+    window.location.href = 'index.html';
+  }
   return movieID;
 }
 
 function getMovieDetails(movieID) {
   $.getJSON(
     'https://www.omdbapi.com/?',
-    { apikey: 'af2beff5', i: movieID },
+    { apikey: ombdAPI_KEY, i: movieID },
     (movieData) => {
       if (movieData.Response != 'True') {
-        console.error(movieData.Error);
-        return;
+        throw movieData.Error;
       }
       document.title = movieData.Title;
       $(poster).attr('src', movieData.Poster);
@@ -53,7 +52,13 @@ function getMovieDetails(movieID) {
       console.log(term);
       $.getJSON(
         'https://www.googleapis.com/youtube/v3/search?',
-        { key: API_KEY, type: 'video', part: 'id', maxResults: 1, q: term },
+        {
+          key: googleAPI_KEY,
+          type: 'video',
+          part: 'id',
+          maxResults: 1,
+          q: term,
+        },
         (trailer) => {
           let videoTag = trailer.items[0].id.videoId;
           $(trailerFrame).attr(
--- scripts/objektet.js
+++ scripts/objektet.js
@@ -1,80 +1,108 @@
-function Person(name,lastname){
-    this.name = name;
-    this.lastname = lastname;
+function Person(name, lastname) {
+  this.name = name;
+  this.lastname = lastname;
 }
-Person.prototype.Pershendetje = function(){
-    console.log("Pershendetje " + this.name + " " + this.lastname);
-}
-var Artin = new Person("Artin", "Sermaxhaj");
+Person.prototype.Pershendetje = function () {
+  console.log('Pershendetje ' + this.name + ' ' + this.lastname);
+};
+var Artin = new Person('Artin', 'Sermaxhaj');
 Artin.Pershendetje();
 
-var Haker = new Person("Haker", "Baba");
+var Haker = new Person('Haker', 'Baba');
 Haker.Pershendetje();
 
-function Kafshe(name, speed){
-    this.name = name;
-    this.speed = speed;
+function Kafshe(name, speed) {
+  this.name = name;
+  this.speed = speed;
 }
-Kafshe.prototype.Leviz = function(){
-    console.log("Kafsha e llojit " + this.name + " leviz me shpejtesi " + this.speed);
+Kafshe.prototype.Leviz = function () {
+  console.log(
+    'Kafsha e llojit ' + this.name + ' leviz me shpejtesi ' + this.speed
+  );
+};
+Kafshe.prototype.Fluturo = function () {
+  throw 'Hakeri osht supernatural po jo deri kit nivel';
+};
+var Haker1 = new Kafshe('Haker', '20km/h');
+
+try {
+  Haker1.Leviz();
+  Haker1.Fluturo();
+} catch (error) {
+  console.error(error);
 }
-var Haker1 = new Kafshe("Haker", "20km/h");
-Haker1.Leviz();
 
 var Automobili = {
-    lloji : "Fiat",
-    viti : 2020,
-    transmisioni : "Automatik",
-    get tipi() {
-        console.log("Automobili eshte i tipit " + this.lloji + " dhe eshte prodhuar ne vitin " + this.viti);
-    }
-    };
+  lloji: 'Fiat',
+  viti: 2020,
+  transmisioni: 'Automatik',
+  get tipi() {
+    console.log(
+      'Automobili eshte i tipit ' +
+        this.lloji +
+        ' dhe eshte prodhuar ne vitin ' +
+        this.viti
+    );
+  },
+};
 
-    Automobili.tipi;
+Automobili.tipi;
 
-    var Shkolla = {
-        lloji : "Mesme",
-        klasa : 3,
-        paralele : 12,
-        nxenesneklase : 30,
-        get nrnxenesvetotal(){
-            console.log( "Nurmi total i nxenesve ne shkolle eshte " + this.nxenesneklase* this.paralele*this.klasa);
-        }
-    }
-    Shkolla.nrnxenesvetotal;
-function Projekt(nxenes, lenda, ora){
-    this.nxenes = nxenes;
-    this.lenda = lenda;
-    this.ora = ora;
+var Shkolla = {
+  lloji: 'Mesme',
+  klasa: 3,
+  paralele: 12,
+  nxenesneklase: 30,
+  get nrnxenesvetotal() {
+    console.log(
+      'Nurmi total i nxenesve ne shkolle eshte ' +
+        this.nxenesneklase * this.paralele * this.klasa
+    );
+  },
+};
+Shkolla.nrnxenesvetotal;
+function Projekt(nxenes, lenda, ora) {
+  this.nxenes = nxenes;
+  this.lenda = lenda;
+  this.ora = ora;
 }
-var Projekti = new Projekt(4, "Interneti",220);
-Projekt.prototype.Mesazh = function(){
-    console.log("Ne projektin nga lenda: " + this.lenda + " moren pjese "+ this.nxenes
-    + " ku ata punuan " + this.ora + " ore.");
-
-}
+var Projekti = new Projekt(4, 'Interneti', 220);
+Projekt.prototype.Mesazh = function () {
+  console.log(
+    'Ne projektin nga lenda: ' +
+      this.lenda +
+      ' moren pjese ' +
+      this.nxenes +
+      ' ku ata punuan ' +
+      this.ora +
+      ' ore.'
+  );
+};
 Projekti.Mesazh();
-function Printo(x1,x2){
-    if(x1 * x2 > Number.MAX_VALUE){
-        console.log("Ky numer nuk mund te kuptohet nga JS.");
-    }
-    else { console.log("Ky numer eshte i perfshire ne domenin e JS.");}
+function Printo(x1, x2) {
+  if (x1 * x2 > Number.MAX_VALUE) {
+    console.log('Ky numer nuk mund te kuptohet nga JS.');
+  } else {
+    console.log('Ky numer eshte i perfshire ne domenin e JS.');
+  }
 }
-console.log(isNaN("asndnasd"));
+console.log(isNaN('asndnasd'));
 console.log(isNaN(123));
 console.log(Printo.toString());
 console.log(Projekt.toString());
-function KuptimiEksponentit(x1){
-    console.log("Numri pa perdorur metoden toExponential(): "+ x1);
-    console.log("Numri me perdorimin e metodes toExponential(): " + x1.toExponential(2));
+function KuptimiEksponentit(x1) {
+  console.log('Numri pa perdorur metoden toExponential(): ' + x1);
+  console.log(
+    'Numri me perdorimin e metodes toExponential(): ' + x1.toExponential(2)
+  );
 }
 KuptimiEksponentit(3123.213213123);
-const p = "Perdorimi i metodes replace";
+const p = 'Perdorimi i metodes replace';
 const regex = /Perdorimi/g;
-console.log(p.replace(regex,"Gabim"));
+console.log(p.replace(regex, 'Gabim'));
 const regex1 = /Perdorimi/i;
-console.log(p.replace(regex1, "Perdorimi i sakte"));
+console.log(p.replace(regex1, 'Perdorimi i sakte'));
 let kaliumi = /Perdorim/s;
-let sps = console.log(kaliumi.exec("Perdorimi i metodes exec ne JS"));
+let sps = console.log(kaliumi.exec('Perdorimi i metodes exec ne JS'));
 let match = /[A-Z]/s;
 console.log(p.match(match));
