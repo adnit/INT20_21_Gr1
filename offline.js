a1f91d6dc1c7cad231824c9b73627e4e724843a4
adniti <adnit.kamberi@student.uni-pr.edu>
2021-01-14 20:13:41 +0100

    fixed offline mode not working as intented

--- offline.js
+++ offline.js
@@ -2,19 +2,21 @@ self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open('kinofikecache').then(function (cache) {
       return cache.addAll([
+        '/',
         '/index.html',
+        '/styles/style.css',
         '/scripts/movies-catalog.js',
         '/scripts/slideshow.js',
         '/scripts/location.js',
         '/scripts/theme.js',
         '/images/movies/TheIrishman.jpg',
+        '/images/favicon.ico',
         '/images/movies/HomeAlone.jpg',
         '/images/movies/Tenet.png',
         '/images/movies/HonestThief.jpeg',
         '/images/movies/Soul.jpeg',
         '/images/movies/Greenland.png',
         '/images/movies/SpenserConfidential.jpg',
-        '/offline.js',
       ]);
     })
   );
